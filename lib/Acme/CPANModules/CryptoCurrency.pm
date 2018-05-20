625ef1772da244abb52b7d6f1a37a47cd4ae0e79
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-05-20 15:23:16 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/CryptoCurrency.pm
@@ -0,0 +1,45 @@
+package Acme::CPANModules::CryptoCurrency;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Cryptocurrency-related modules",
+    entries => [
+
+        # these are from CPANModules-CryptoExchange-API
+        {
+            module => 'WebService::Cryptopia',
+        },
+        {
+            module => 'WebService::Binance',
+        },
+        {
+            module => 'Finance::BTCIndo',
+        },
+        {
+            module => 'Finance::BitFlip',
+        },
+        {
+            module => 'Finance::Bank::Kraken',
+        },
+        {
+            module => 'Poloniex::API',
+        },
+        {
+            module => 'WWW::API::Bitfinex',
+        },
+        {
+            module => 'Finance::BitStamp::API',
+        },
+        {
+            module => 'Finance::GDAX::API',
+        },
+        {
+            module => 'Finance::LocalBitcoins::API',
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
